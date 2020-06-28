75a43a0bae5ecb8fcec56b29a48caa091b001f86
BangYourGang <dvyanshu@iitk.ac.in>
2020-06-29 03:04:58 +0530

    Generate a trajectory on the basis of given constraints from mav's start point to first pylon

---
+++ iarc_trajectory_estimation/include/iarc_trajectory_estimation/trajectory_generation.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <ros/ros.h>
+#include <mav_trajectory_generation/polynomial_optimization_linear.h>
+#include <mav_trajectory_generation_ros/ros_visualization.h>
+#include <mav_trajectory_generation/trajectory.h>
+#include <mav_trajectory_generation/trajectory_sampling.h>
+#include <nav_msgs/Odometry.h>
+#include <trajectory_msgs/MultiDOFJointTrajectory.h>
+#include <mav_msgs/conversions.h>
+// #include <mav_msgs/eigen_mav_msgs.h>
+#include <Eigen/Dense>
+#include <std_srvs/Trigger.h>
+
+namespace ariitk::trajectory_generation
+{
+    class TrajectoryGenerationPolynomial
+    {
+    public:
+        TrajectoryGenerationPolynomial(const ros::NodeHandle &nh, const ros::NodeHandle &nh_private);
+        ~TrajectoryGenerationPolynomial() {}
+        void run();
+
+    private:
+        void mavOdometryCallback(const nav_msgs::Odometry &msg);
+        void computePoints();
+        void generateTrajectory();
+        bool commandServiceCallback(std_srvs::Trigger::Request &req, std_srvs::Trigger::Response &resp);
+
+        bool visualize_;
+        bool command_;
+
+        double v_max_;
+        double a_max_;
+        double distance_;
+
+        int dimension_;
+        int derivative_to_optimize_;
+
+        bool publish_;
+
+        std::vector<double> segment_times_;
+
+        mav_trajectory_generation::Vertex::Vector vertices_;
+        mav_trajectory_generation::Trajectory trajectory_;
+
+        nav_msgs::Odometry mav_odom_;
+        mav_msgs::EigenTrajectoryPoint::Vector trajectory_points_;
+        trajectory_msgs::MultiDOFJointTrajectory generated_trajectory_;
+        visualization_msgs::MarkerArray markers_;
+
+        ros::NodeHandle nh_;
+        ros::NodeHandle nh_private_;
+
+        ros::Subscriber mav_odom_sub_;
+        ros::Publisher mav_odom_pub_;
+        ros::Publisher marker_pub_;
+        ros::Publisher trajectory_pub_;
+        ros::ServiceServer server_;
+        ros::ServiceClient client_;
+        
+        // std_srvs::Trigger srv_;
+    };
+
+} // namespace ariitk::trajectory_generation
---
+++ iarc_trajectory_estimation/src/trajectory_generation_node.cpp
@@ -0,0 +1,21 @@
+#include <iarc_trajectory_estimation/trajectory_generation.hpp>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "trajectory_generator");
+    ros::NodeHandle nh("");
+    ros::NodeHandle nh_private("~");
+
+    ariitk::trajectory_generation::TrajectoryGenerationPolynomial trajectory(nh, nh_private);
+    int rate;
+    nh_private.param("rate", rate, 10);
+    ros::Rate loop_rate(rate);
+
+    while (ros::ok())
+    {
+        trajectory.run();
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+}

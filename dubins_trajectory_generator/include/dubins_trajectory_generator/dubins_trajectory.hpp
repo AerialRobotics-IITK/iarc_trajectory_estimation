5c1a39adc9043b5c0b6695b54e1eca77788a36fd
kpiyush25 <piyushk@iitk.ac.in>
2020-06-28 11:45:22 +0530

    Include class Color and visualization markers

--- dubins_trajectory_generator/include/dubins_trajectory_generator/dubins_trajectory.hpp
+++ dubins_trajectory_generator/include/dubins_trajectory_generator/dubins_trajectory.hpp
@@ -2,9 +2,15 @@
 
 #include <ros/ros.h>
 #include <memory>
+#include <mav_planning_common/utils.h>
+#include <mav_planning_common/path_visualization.h>
+#include <mav_planning_common/physical_constraints.h>
+#include <mav_msgs/conversions.h>
+#include <mav_visualization/helpers.h>
 #include <ros/package.h>
 #include <eigen3/Eigen/Dense>
 #include <visualization_msgs/MarkerArray.h>
+
 
 namespace ariitk::trajectory_generation {
 
@@ -15,11 +21,39 @@ struct Point {
     typedef Point::Ptr Node;
 };
 
+class Color : public std_msgs::ColorRGBA {
+    public:
+    Color() : std_msgs::ColorRGBA() {}
+    Color(double red, double green, double blue) : Color(red, green, blue, 1.0) {}
+    Color(double red, double green, double blue, double alpha) : Color() {
+        r = red;
+        g = green;
+        b = blue;
+        a = alpha;
+    }
+
+    static const Color White() { return Color(1.0, 1.0, 1.0); }
+    static const Color Black() { return Color(0.0, 0.0, 0.0); }
+    static const Color Gray() { return Color(0.5, 0.5, 0.5); }
+    static const Color Red() { return Color(1.0, 0.0, 0.0); }
+    static const Color Green() { return Color(0.0, 1.0, 0.0); }
+    static const Color Blue() { return Color(0.0, 0.0, 1.0); }
+    static const Color Yellow() { return Color(1.0, 1.0, 0.0); }
+    static const Color Orange() { return Color(1.0, 0.5, 0.0); }
+    static const Color Purple() { return Color(0.5, 0.0, 1.0); }
+    static const Color Chartreuse() { return Color(0.5, 1.0, 0.0); }
+    static const Color Teal() { return Color(0.0, 1.0, 1.0); }
+    static const Color Pink() { return Color(1.0, 0.0, 0.5); }
+};
+
 class DubinsTrajectory {
     public:
     DubinsTrajectory(const ros::NodeHandle& nh,const ros::NodeHandle& nh_private);
-    void generateTrajectory(std::vector<Point> trajectory);
-    void init();
+    void generateTrajectory(Point start, Point end);
+    enum class ColorType{WHITE, BLACK, GRAY, RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CHARTREUSE, TEAL, PINK};
+    void visualizeTrajectory(const std::string& topic_name, std::vector<Point> trajectory, 
+                                    const std::string& frame_id = "world", const ColorType& color = ColorType::PINK, const double& size_factor= 0.5);
+    void init(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private);
     void run();
 
     private:
@@ -30,6 +64,7 @@ class DubinsTrajectory {
     double a_max_;
     double turn_in_one_segment_;
     double curvature_; 
+    double size_factor_;
     double distance_; //distance between the centres of two pylons.
     int num_arc_; 
     int num_straight_;
@@ -40,7 +75,8 @@ class DubinsTrajectory {
     Eigen::Vector3d pylon_two_;
     Point start_;
     Point end_;
-    visualization_msgs::MarkerArray markers_;
+    std::unordered_map<ColorType, Color> color_map_;
+    // visualization_msgs::MarkerArray markers_;
     std::vector<Point> trajectory_;
     ros::Publisher marker_pub_;
 
